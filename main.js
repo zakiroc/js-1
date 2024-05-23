fef8564daebbfa12b4c62cd87e1902f2cca488fc
zakiroc <164227048+zakiroc@users.noreply.github.com>
2024-05-23 14:34:31 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let firstname = prompt('ism ')
+let lastname = prompt('familiya')
+let age = prompt('yoshingiz')
+let region = prompt ('yashayotgan joyiz')
+let subject = prompt ('siz yoqtirgan darsiz')
+
+
+alert('malumot uchun rahmat')
+
+console.log("ism:",firstname )
+console.log("familiya:",lastname)
+console.log("yosh:",age)
+console.log('yashayotgan joy:',region)
+console.log("dars:",subject)
